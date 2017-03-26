41a0d1dfc66a9141cc79d45ffc9f676cc5cff7ff
lipheris <lipheris@gmail.com>
2017-03-26 16:23:32 +0800

    將Picture的統計資料stats改成Enbedded, Stats從Entity改成Embeddable

--- src/main/java/tw/com/iwow/model/entity/Picture.java
+++ src/main/java/tw/com/iwow/model/entity/Picture.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -14,7 +15,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -29,7 +29,7 @@ public class Picture {
 	private String name;
 	@Column(name = "date_update") // database column 好像禁用update 所以使用date_update
 	private LocalDateTime update;
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(optional = false, fetch = FetchType.LAZY)
 	@JoinColumn(name = "UP_ID")
 	private Member uploader;
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "picture")
@@ -37,8 +37,7 @@ public class Picture {
 	private boolean visibility;// visibility 為區分公開/ 私人
 	@Column(name = "file_p")
 	private Blob file;
-	@OneToOne(orphanRemoval = true, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	@JoinColumn(name = "STA_ID")
+	@Embedded
 	private Stats stats;
 	@OneToMany(orphanRemoval = true, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "PIC_ID", referencedColumnName = "ID")
--- src/main/java/tw/com/iwow/model/entity/Stats.java
+++ src/main/java/tw/com/iwow/model/entity/Stats.java
@@ -1,26 +1,18 @@
 package tw.com.iwow.model.entity;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-@Entity
-@Table(name="STATS")
+import javax.persistence.Embeddable;
 
-//Stats 為針對每張圖之統計資料 ,如購買明細/每月銷售額
-public class Stats  {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(unique = true, nullable = false)
-	private Long id;
-	@Column(name="sal_tatal")
-	private Double salesTotal;//salesTotal 為針對每張圖之總銷售額
-	@Column(name="q_sal_total")
-	private Double quaSalesTotal;//quaSalesTotal 為針對每張圖之總銷次數
-//	@Column(name="SAL_MONTHLY")
-//	private Double salesMonthly;
-//	@Column(name="SAL_WEEKLY")
-//	private Double salesWeekly;
+@Embeddable
+// Stats 為針對每張圖之統計資料 ,如購買明細/每月銷售額
+public class Stats {
+
+	@Column(name = "sal_tatal")
+	private Double salesTotal;// salesTotal 為針對每張圖之總銷售額
+	@Column(name = "q_sal_total")
+	private Double quaSalesTotal;// quaSalesTotal 為針對每張圖之總銷次數
+	// @Column(name="SAL_MONTHLY")
+	// private Double salesMonthly;
+	// @Column(name="SAL_WEEKLY")
+	// private Double salesWeekly;
 }
