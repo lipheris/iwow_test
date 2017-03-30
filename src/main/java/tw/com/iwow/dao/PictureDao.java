a8b4dc2c614103e52500a17b552f882a23020ad7
王廷倫 <FaGao@EEIT>
2017-03-30 17:04:48 +0800

    PictureDao加上findByFile

--- src/main/java/tw/com/iwow/dao/PictureDao.java
+++ src/main/java/tw/com/iwow/dao/PictureDao.java
@@ -1,5 +1,7 @@
 package tw.com.iwow.dao;
 
+import java.sql.Blob;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import tw.com.iwow.entity.Picture;
 @Repository
 public interface PictureDao extends JpaRepository<Picture, Long> {
 
+	public Picture findByFile(Blob file);
 }
