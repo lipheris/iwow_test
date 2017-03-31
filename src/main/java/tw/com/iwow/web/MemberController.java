b5d30cf04b9cf5544f0a3fc0b53d6f9b5d676532
王廷倫 <FaGao@EEIT>
2017-03-31 21:14:11 +0800

    MemberController加入getByEmail

--- src/main/java/tw/com/iwow/web/MemberController.java
+++ src/main/java/tw/com/iwow/web/MemberController.java
@@ -2,17 +2,35 @@ package tw.com.iwow.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import tw.com.iwow.entity.Member;
 import tw.com.iwow.service.MemberService;
 
 @Controller
-@RequestMapping(value="/iwow")
+@RequestMapping(value="/iwow/member")
 public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
+	
+//	@RequestMapping(value="/edit")
+//	public String editPage(Model model, Member form){
+//		Long id = form.getId();
+//		Member member = memberService.findById(id);
+//		model.addAttribute("member",member);
+//		return "iwow/setting_profile";
+//	}
+	
+	@RequestMapping(value="/getByEmail")
+	public String getdByEmailPage(Model model, Member form){
+		String email = form.getEmail();
+		System.out.println(email);
+		Member member = memberService.getByEmail(email);
+		model.addAttribute("member",member);
+		return "iwow/setting_profile";
+	}
 	
 	@RequestMapping(value="/insert")
 	public String insert(Member member){
