7dc7c0d04b7b835b68a767771bc26ebd63799b4d
王廷倫 <FaGao@EEIT>
2017-04-06 21:32:58 +0800

    日期轉型

--- src/main/java/tw/com/iwow/web/MemberController.java
+++ src/main/java/tw/com/iwow/web/MemberController.java
@@ -1,17 +1,31 @@
 package tw.com.iwow.web;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import tw.com.iwow.entity.Member;
+import tw.com.iwow.entity.Picture;
 import tw.com.iwow.service.MemberService;
+import tw.com.iwow.service.PictureService;
 
 @RequestMapping(value="/iwow/member")
 @Controller
@@ -20,6 +34,8 @@ public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
+	@Autowired
+	private PictureService pictureService;
 	
 	@RequestMapping(value="/checkUserEmail")
 	@ResponseBody
@@ -45,10 +61,14 @@ public class MemberController {
 	 }
 		
 	@RequestMapping(value="/insert")
-	public String insert(Member member, Model model){
+	public String insert(Member member,BindingResult bindingResult, Model model,String birth){
 		
+		System.out.println(birth);
+		DateTimeFormatter dtf =  DateTimeFormatter.ofPattern("MM/dd/yyyy");
+		LocalDate ld=LocalDate.parse(birth, dtf);				
+		LocalDateTime ldt = LocalDateTime.of(ld, LocalTime.MIN);
+		member.setBirth(ldt);
 		Member tempMen = memberService.insert(member);
-		
 		if(tempMen == null){
 			Map<String,String> errorMsg = new HashMap<String,String>();
 			errorMsg.put("error", "error");
@@ -69,4 +89,51 @@ public class MemberController {
 		memberService.update(member);
 		return "redirect:/iwow/index_user";
 	}
+	
+	// CollectionList頁面
+		@RequestMapping("/collectionlist")
+		public String wishListPage( Model model) {
+			String currentPrincipalName = SecurityContextHolder.getContext().getAuthentication().getName();
+			Long Id = memberService.getByEmail(currentPrincipalName).getId();
+			Member member = memberService.findById(Id);
+			Set<Picture> collectionList = member.getPicColls();
+			model.addAttribute("collectionList", collectionList);
+			model.addAttribute("member", member);
+			return "/iwow/collectionlist";
+		}
+
+		// insert Collection
+		@RequestMapping("/collect/picture/{picId}")
+		@ResponseBody
+		public Boolean wishListInsert(@PathVariable Long picId, Model model) {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			String currentPrincipalName = authentication.getName();
+			Long Id = memberService.getByEmail(currentPrincipalName).getId();
+			Member member = memberService.findById(Id);
+			Picture picture = pictureService.getById(picId);
+			Set<Picture> pictures = member.getPicColls();
+			if (pictures.contains(picture)) {
+				return false;
+			}
+			pictures.add(picture);
+			member.setPicColls(pictures);
+			memberService.update(member);
+			return true;
+		}
+
+		// delete Collection
+		@RequestMapping("/collect/picture/delete")
+		@ResponseBody
+		public Integer wishListDelete(@RequestParam Long picId, HttpServletRequest request, Model model) {
+			Long Id = (Long) request.getSession().getAttribute("Id");
+			Member member = memberService.findById(Id);
+			Set<Picture> pictures = member.getPicColls();
+			Picture picture = pictureService.getById(picId);
+			if (pictures.contains(picture)) {
+				pictures.remove(picture);
+				member.setPicColls(pictures);
+				memberService.update(member);
+			}
+			return pictures.size();
+		}
 }
