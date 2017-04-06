64d7e302dc68be80d20a0e26463743aab5016d4a
王廷倫 <FaGao@EEIT>
2017-04-06 20:05:15 +0800

    MemberController驗證信箱是否已存在，insert成功/失敗頁面導向與判別警告視窗是否要跳出

--- src/main/java/tw/com/iwow/web/MemberController.java
+++ src/main/java/tw/com/iwow/web/MemberController.java
@@ -1,23 +1,30 @@
 package tw.com.iwow.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import tw.com.iwow.entity.Member;
 import tw.com.iwow.service.MemberService;
 
 @RequestMapping(value="/iwow/member")
-@RestController
+@Controller
+@SessionAttributes(names={"correctMsg"})
 public class MemberController {
 	
 	@Autowired
 	private MemberService memberService;
 	
 	@RequestMapping(value="/checkUserEmail")
+	@ResponseBody
 	public String checkUserEmail(Member form){
-		String UserEmail = form.getEmail(); //取得使用者輸入的Email		
+		String UserEmail = form.getEmail(); //取得使用者輸入的Email	
 		if(UserEmail != null && UserEmail.trim().length() != 0){ //檢查使用者輸入的Email不為null與空白
 			Member member = memberService.getByEmail(UserEmail); //測試使用者輸入的email是否可抓取資料庫內的資料
 			if(member == null){ //如果member為null代表資料庫內沒該筆email，帳號可註冊
@@ -29,22 +36,36 @@ public class MemberController {
 		return null;
 	}
 	
-	@RequestMapping(value="/getByEmail")
-	public String getdByEmailPage(Model model, Member form){
+	@RequestMapping(value="/edit")
+	 public String editPage(Model model, Member form){
 		String email = form.getEmail(); //取得使用者輸入的email(securrity的username)
 		Member member = memberService.getByEmail(email); //從資料庫內抓取該email的資料
-		model.addAttribute("member",member);
-		return "iwow/setting_profile";
-	}
-	
+	 	model.addAttribute("editMember",member);
+	 	return "iwow/setting_profile";
+	 }
+		
 	@RequestMapping(value="/insert")
-	public String insert(Member member){
-		memberService.insert(member);
+	public String insert(Member member, Model model){
+		
+		Member tempMen = memberService.insert(member);
+		
+		if(tempMen == null){
+			Map<String,String> errorMsg = new HashMap<String,String>();
+			errorMsg.put("error", "error");
+			model.addAttribute("errorMsg",errorMsg);
+			return "/iwow/signup";
+		} 
+
+		Map<String,String> correctMsg = new HashMap<String,String>(); 
+		correctMsg.put("correct", "correct");
+		model.addAttribute("correctMsg",correctMsg);
+		
 		return "redirect:/iwow/index_user";
 	}
 	
 	@RequestMapping(value="/update")
 	public String update(Member member){
+		Member mem=memberService.findById(member.getId());
 		memberService.update(member);
 		return "redirect:/iwow/index_user";
 	}
