10ea7fba4d447266aff59c989949df65b8dfb1f6
lipheris <lipheris@gmail.com>
2017-04-06 11:40:00 +0800

    conflict

--- src/main/java/tw/com/iwow/service/PictureService.java
+++ src/main/java/tw/com/iwow/service/PictureService.java
@@ -1,11 +1,22 @@
 package tw.com.iwow.service;
 
-import java.sql.Blob;
+import java.security.Principal;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+
+import tw.com.iwow.dao.MemberDao;
 import tw.com.iwow.dao.PictureDao;
 import tw.com.iwow.dao.TagDao;
 import tw.com.iwow.entity.Picture;
@@ -14,17 +25,46 @@ import tw.com.iwow.entity.Tag;
 @Service
 public class PictureService {
 
+    private static final String storageConnectionString =
+			"DefaultEndpointsProtocol=https;AccountName=iwowblob;AccountKey=aSzX3lBzin0MehMzxDUo0jULu2A7PhtrH+0WxEaFuyj1AwpXwnwjkcOLM3BJwBFKZNVxDGza8f4t4JNcUlQNUA==" +
+					"AccountName=iwowblob;" +
+					"AccountKey=aSzX3lBzin0MehMzxDUo0jULu2A7PhtrH+0WxEaFuyj1AwpXwnwjkcOLM3BJwBFKZNVxDGza8f4t4JNcUlQNUA==";
 	@Autowired
 	private PictureDao pictureDao;
 	@Autowired
 	private TagDao tagDao;
+	@Autowired
+	private MemberDao memberDao;
 
 	public Picture findById(Long id) {
 		return pictureDao.findOne(id);
 	}
 
-	public Picture insert(Picture picture) {
-		return pictureDao.save(picture);
+	public Picture insert(Picture picture,CommonsMultipartFile file) {
+		//圖片上傳到雲端的語法
+		try{
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			String currentPrincipalName = authentication.getName();
+			Long uploaderId = memberDao.findByEmail(currentPrincipalName).getId();
+			  // Retrieve storage account from connection-string.	和microsoft取得連線
+		    CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+
+		    // Create the blob client.
+		    CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+		    // Retrieve reference to a previously created container.		選擇到哪一個container
+		    CloudBlobContainer container = blobClient.getContainerReference("mycontainer");
+
+		    // Create or overwrite the "myimage.jpg" blob with contents from a local file.	存取為什麼檔案
+		    CloudBlockBlob blob = container.getBlockBlobReference(picture.getName().trim()+uploaderId+".jpg");
+		    blob.upload(file.getInputStream(), file.getSize());
+		    picture.setPictureAddress("https://iwowblob.blob.core.windows.net/mycontainer/"+picture.getName().trim()+uploaderId+".jpg");
+		    picture.setUploaderId(uploaderId);
+			return pictureDao.save(picture);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public Picture update(Picture picture) {
@@ -39,8 +79,8 @@ public class PictureService {
 		return pictureDao.findAll();
 	}
 
-	public Picture getByFile(Blob file) {
-		return pictureDao.findByFile(file);
+	public Picture getByFile(String file) {
+		return pictureDao.findByPictureAddress(file);
 	}
 
 	public void addTag(Picture picture, String tagName) {
@@ -50,5 +90,13 @@ public class PictureService {
 			tag.setName(tagName);
 		}
 		picture.addTag(tag);
+	}
+	public Set<Long> search(String param){
+		Set<Long> result = new HashSet<>();
+		Set<Object> temp = pictureDao.search(param);
+		for(Object obj:temp){
+			result.add((Long)obj);
+		}
+		return result;
 	}
 }
--- src/main/java/tw/com/iwow/web/IwowController.java
+++ src/main/java/tw/com/iwow/web/IwowController.java
@@ -1,20 +1,24 @@
 package tw.com.iwow.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.SystemPropertyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import tw.com.iwow.entity.Picture;
 import tw.com.iwow.service.PictureService;
 
 @Controller
 public class IwowController {
+	@Autowired
 	private PictureService picService;
 	// iwowwow
 	@RequestMapping(value = "/iwow/index")
@@ -26,7 +30,7 @@ public class IwowController {
 	public String picturePage() {
 		return "iwow/picture";
 	}
-
+	
 	@RequestMapping(value = "/iwow/login")
 	public String loginPage() {
 		return "iwow/index_user";
@@ -43,10 +47,11 @@ public class IwowController {
 	}
 
 	@RequestMapping(value = "/iwow/search", method = RequestMethod.GET)
-	public String searchPage(@RequestParam(value="searchContext")String param, Model model) {
+	public String searchPage(@RequestParam(value="searchCtx")String param, Model model) {
+		System.out.println("123");
 		if(param.isEmpty()||param==null)
 			return "iwow/index";
-		
+		model.addAttribute("resultId", picService.search(param));
 		return "iwow/search";
 	}
 
@@ -100,4 +105,5 @@ public class IwowController {
 		return "iwow/test";
 
 	}
+	
 }
