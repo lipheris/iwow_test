aa2046af6e62561d2cec0c9bdd8bd173c7c519ba
Aaben <qazwsx241jp@gmail.com>
2017-04-09 10:02:54 +0800

    Group fix

--- src/main/java/tw/com/iwow/web/GroupController.java
+++ src/main/java/tw/com/iwow/web/GroupController.java
@@ -1,13 +1,28 @@
 package tw.com.iwow.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import tw.com.iwow.entity.Group;
+import tw.com.iwow.service.GroupService;
 
 @Controller
 @RequestMapping(value="/iwow/member/group")
 public class GroupController {
-	@RequestMapping
-	public String createGroup(){
+	
+	@Autowired
+	public GroupService groupService;
+	
+	@RequestMapping(value="/create")
+	public String createGroup(Group group){
+		groupService.create(group);
+		return "/iwow/member/group";
+	}
+	
+	@RequestMapping(value="")
+	public String deleteGroup(Long id){
+		groupService.delete(id);
 		return "";
 	}
 }
--- src/test/java/iwow_test/GroupServiceTest.java
+++ src/test/java/iwow_test/GroupServiceTest.java
@@ -1,6 +1,7 @@
 package iwow_test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +11,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import tw.com.iwow.dao.GroupDao;
+import tw.com.iwow.dao.MemberDao;
 import tw.com.iwow.entity.Group;
+import tw.com.iwow.entity.Member;
 import tw.com.iwow.service.GroupService;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/spring-context.xml"})
@@ -21,17 +24,37 @@ public class GroupServiceTest {
 	public GroupDao groupDao;
 	@Autowired
 	public GroupService groupService;
+	@Autowired
+	public MemberDao memberDao;
 	
 	@Test
 	public void test() {
 		Group group = new Group();
 		group.setName("cat");
+		groupDao.save(group);	
+//		Group group2=groupDao.findByName("cat");
+//		assertEquals(group, group2);
 		
-		groupDao.save(new Group());
-		Group group2=groupDao.findByName("cat");
-		assertEquals(group, group2);
-		
+		Set<Group> groups = new  HashSet<Group>();
+		Group group2 =new Group();
+		group2.setName("animal");
+		groupDao.save(group2);
+		groups.add(group);
+		groups.add(group2);
+		Set<Member> members = new HashSet<Member>();
+		Member member = new Member();
+		member.setGroups(groups);
+		member.setName("Qq");
+		memberDao.save(member);
+		members.add(member);
+		group2.setMembers(members);
+//		Member member2 = new Member();
+//		member2.setGroups(groups);
+//		member2.setName("Kk");
+//		memberService.insert(member2);
+		System.out.println( groupService.getById(member.getId()));
 		
 	}
+	
 
 }
