e695b1fc5043eaabad6b79db82f1649c1ac3180a
Aaben <qazwsx241jp@gmail.com>
2017-04-10 10:58:10 +0800

    signup 去掉banner 新增 RoleDao 新增 ManytoMany mappedby roles

--- src/main/java/tw/com/iwow/entity/Role.java
+++ src/main/java/tw/com/iwow/entity/Role.java
@@ -1,10 +1,14 @@
 package tw.com.iwow.entity;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,6 +20,17 @@ public class Role {
 	private Long id;
 	@Column(name = "AUTH")
 	private String auth;
+	
+	@ManyToMany(fetch = FetchType.LAZY,mappedBy = "roles")
+	private Set<Member> members ;
+
+	public Set<Member> getMembers() {
+		return members;
+	}
+
+	public void setMembers(Set<Member> members) {
+		this.members = members;
+	}
 
 	public Long getId() {
 		return id;
