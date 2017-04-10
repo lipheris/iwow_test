a5b5af0145f799278451d200d0c41899bd5f8bc6
lipheris <lipheris@gmail.com>
2017-04-10 20:41:39 +0800

    error:當 IDENTITY_INSERT 設為 OFF 時，無法將外顯值插入資料表 'STATS' 的識別資料行中。 先不自動產生stats

--- src/main/java/tw/com/iwow/entity/Picture.java
+++ src/main/java/tw/com/iwow/entity/Picture.java
@@ -24,7 +24,6 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import com.google.gson.annotations.Expose;
@@ -78,8 +77,8 @@ public class Picture {
 	}
 
 	@OneToOne(cascade = CascadeType.ALL)
-	@PrimaryKeyJoinColumn(name = "ID", referencedColumnName = "ID")
-	private Stats stats = new Stats(this);
+	@PrimaryKeyJoinColumn
+	private Stats stats;// = new Stats(this);
 	@JsonIgnore
 	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "PIC_ID", referencedColumnName = "ID")
