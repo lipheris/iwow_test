91da8d38fe8041eb173c3818596234ca32642402
Aaben <qazwsx241jp@gmail.com>
2017-04-19 16:34:53 +0800

    characterEncoding

--- src/main/java/tw/com/iwow/config/DispatcherServletConfig.java
+++ src/main/java/tw/com/iwow/config/DispatcherServletConfig.java
@@ -1,5 +1,8 @@
 package tw.com.iwow.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -18,5 +21,25 @@ public class DispatcherServletConfig extends AbstractAnnotationConfigDispatcherS
 	protected String[] getServletMappings() {
 		return new String[]{"/"};
 	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+	    characterEncodingFilter.setEncoding("UTF-8");
+	    characterEncodingFilter.setForceEncoding(true);
+	    return new Filter[] { characterEncodingFilter};		
+	}
+
+//	@Override
+//	public void onStartup(ServletContext servletContext) throws ServletException {
+//		FilterRegistration.Dynamic encodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+//		encodingFilter.setInitParameter("encoding", "UTF-8");
+//		encodingFilter.setInitParameter("forceEncoding", "true");
+//		encodingFilter.addMappingForUrlPatterns(null, true, "/*");		
+//	}
+
+
+	
+	
 
 }
